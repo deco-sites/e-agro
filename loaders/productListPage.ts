66bb5c6dfc55411ad6bf345d97ee46bb2df74523
lui-dias <github.anyplace863@passinbox.com>
2024-11-21 03:01:47 -0300

    remove console.log

--- loaders/productListPage.ts
+++ loaders/productListPage.ts
@@ -1,35 +1,35 @@
-import type { Product } from "apps/commerce/types.ts";
-import type { AppContext } from "site/apps/site.ts";
+import type { Product } from 'apps/commerce/types.ts'
+import type { AppContext } from 'site/apps/site.ts'
 
 /**
  * @title {{key}} - {{value}}
  */
 interface FilterParams {
-  key: string;
-  value: string;
+    key: string
+    value: string
 }
 
 interface Props {
-  // filtersToShow: FilterToShowItem[];
-  // unavailableThreshold: number;
+    // filtersToShow: FilterToShowItem[];
+    // unavailableThreshold: number;
 
-  /**
-   * @default 36
-   */
-  limit: number;
+    /**
+     * @default 36
+     */
+    limit: number
 
-  /**
-   * @default 16
-   */
-  mobileLimit: number;
+    /**
+     * @default 16
+     */
+    mobileLimit: number
 
-  /**
-   * @title Items per page default
-   * @description number of products per page to display
-   */
-  count: number;
+    /**
+     * @title Items per page default
+     * @description number of products per page to display
+     */
+    count: number
 
-  filtersParams: FilterParams[];
+    filtersParams: FilterParams[]
 }
 
 /**
@@ -37,58 +37,38 @@ interface Props {
  * @description Product List Page loader
  */
 export default async function loader(
-  { filtersParams = [], count = 12 }: Props,
-  _req: Request,
-  ctx: AppContext,
+    { filtersParams = [], count = 12 }: Props,
+    _req: Request,
+    ctx: AppContext,
 ): Promise<Product[] | null> {
-  const productsData = (await ctx.invoke.site.loaders.productsFromJson()) ?? [];
+    const productsData = (await ctx.invoke.site.loaders.productsFromJson()) ?? []
 
-  if (!productsData.length) return null;
+    if (!productsData.length) return null
 
-  const filtersParamsRecord = filtersParams?.reduce(
-    (acc, { key, value }) => {
-      acc[key] = value;
-      return acc;
-    },
-    {} as Record<string, string>,
-  );
+    const filtersParamsRecord = filtersParams?.reduce(
+        (acc, { key, value }) => {
+            acc[key] = value
+            return acc
+        },
+        {} as Record<string, string>,
+    )
 
-  const { products } = getFilteredProducts(
-    productsData,
-    filtersParamsRecord,
-    count,
-  );
+    const { products } = getFilteredProducts(productsData, filtersParamsRecord, count)
 
-  return products;
+    return products
 }
 
-const getFilteredProducts = (
-  products: Product[],
-  filtersParams: Record<string, string>,
-  count: number,
-) => {
-  const filteredProducts = products.filter((product) => {
-    return Object.entries(filtersParams).every(([key, value]) => {
-      console.log(
-        key,
-        value,
-        product.additionalProperty?.some((property) =>
-          property.name === key && property.value === value
-        ),
-      );
+const getFilteredProducts = (products: Product[], filtersParams: Record<string, string>, count: number) => {
+    const filteredProducts = products.filter(product => {
+        return Object.entries(filtersParams).every(([key, value]) => {
+            return product.additionalProperty?.some(property => property.name === key && property.value === value)
+        })
+    })
 
-      return product.additionalProperty?.some((property) =>
-        property.name === key && property.value === value
-      );
-    });
-  });
+    const properties = filteredProducts.flatMap(product => product.additionalProperty).filter(property => !!property)
 
-  const properties = filteredProducts.flatMap((product) =>
-    product.additionalProperty
-  ).filter((property) => !!property);
-
-  return {
-    products: filteredProducts.slice(0, count),
-    properties,
-  };
-};
+    return {
+        products: filteredProducts.slice(0, count),
+        properties,
+    }
+}
